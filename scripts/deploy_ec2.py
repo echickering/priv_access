a87866d484583c49863932a8bb069c38f577550b
echickering <echickering@gmail.com>
2024-01-28 21:21:11 -0500

    resolve ec2 deployment userdata

--- scripts/deploy_ec2.py
+++ scripts/deploy_ec2.py
@@ -22,8 +22,17 @@ def create_security_groups(ec2_client, vpc_id, name_prefix):
         Description="Public Security Group",
         VpcId=vpc_id
     )
-    # Add public security group rules (modify as needed)
-    # ec2_client.authorize_security_group_ingress(...)
+
+    # Add public security group rules
+    ec2_client.authorize_security_group_ingress(
+        GroupId=sg_public['GroupId'],
+        IpPermissions=[
+            {'IpProtocol': 'tcp', 'FromPort': 443, 'ToPort': 443, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'udp', 'FromPort': 500, 'ToPort': 500, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'udp', 'FromPort': 4500, 'ToPort': 4500, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'udp', 'FromPort': 4501, 'ToPort': 4501, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+        ]
+    )
 
     # Create a private security group
     sg_private = ec2_client.create_security_group(
@@ -31,8 +40,15 @@ def create_security_groups(ec2_client, vpc_id, name_prefix):
         Description="Private Security Group",
         VpcId=vpc_id
     )
-    # Add private security group rules (modify as needed)
-    # ec2_client.authorize_security_group_ingress(...)
+
+    # Add private security group rules (Allowing all RFC1918 traffic)
+    for cidr in ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']:
+        ec2_client.authorize_security_group_ingress(
+            GroupId=sg_private['GroupId'],
+            IpPermissions=[
+                {'IpProtocol': '-1', 'FromPort': 0, 'ToPort': 65535, 'IpRanges': [{'CidrIp': cidr}]}
+            ]
+        )
 
     return sg_public['GroupId'], sg_private['GroupId']
 
@@ -58,21 +74,28 @@ def associate_elastic_ip(ec2_client, allocation_id, network_interface_id):
     )
 
 # Function to create and configure EC2 instances
-def deploy_ec2_instance(ec2_client, ami_id, instance_type, key_name, user_data, network_interfaces):
+def deploy_ec2_instance(ec2_client, ami_id, instance_type, key_name, user_data, network_interfaces_config, instance_name):
     # Specify the block device mapping (EBS volumes)
     block_device_mappings = [{
-        'DeviceName': '/dev/xvda1',  # Adjust this based on AMI default
+        'DeviceName': '/dev/xvda',  # Adjust this based on AMI default
         'Ebs': {
             'VolumeSize': 60,  # Size in GB
             'VolumeType': 'gp3',
             'Encrypted': True,
-            'DeleteOnTermination': True  # EBS volume is deleted on instance termination
+            'DeleteOnTermination': True
         },
     }]
 
-    # Update each network interface to set DeleteOnTermination
-    for interface in network_interfaces:
-        interface.update({'DeleteOnTermination': True})
+    # Specify network interfaces
+    network_interfaces = []
+    for i, ni_config in enumerate(network_interfaces_config):
+        network_interface = {
+            'DeviceIndex': i,
+            'SubnetId': ni_config['SubnetId'],
+            'Groups': [ni_config['SecurityGroupId']],
+            'DeleteOnTermination': True
+        }
+        network_interfaces.append(network_interface)
 
     # Launch the EC2 instance
     instance = ec2_client.run_instances(
@@ -84,13 +107,22 @@ def deploy_ec2_instance(ec2_client, ami_id, instance_type, key_name, user_data, 
         UserData=user_data,
         BlockDeviceMappings=block_device_mappings,
         NetworkInterfaces=network_interfaces,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'Name', 'Value': instance_name}
+                ]
+            }
+        ],
         MetadataOptions={'HttpTokens': 'required'}
     )
     return instance['Instances'][0]['InstanceId']
 
 def main():
     config = load_config('./config/config.yml')
-    state = {}
+    state = {} #Dictionary to keep track of Resource ID in a state file
+    vm_count = {}  # Dictionary to keep track of VM count per region
 
     for region, region_config in config['aws']['Regions'].items():
         ec2_client = boto3.client('ec2', region_name=region)
@@ -100,54 +132,77 @@ def main():
         stack_name = config['aws']['StackName']
         region_outputs = get_cf_outputs(cf_client, stack_name)
 
-        # Prepare user_data by replacing placeholders
-        user_data_raw = config['aws']['EC2']['user_data'].format(
-            NamePrefix=config['aws']['NamePrefix'],
+        # Increment VM count for the region
+        vm_count[region] = vm_count.get(region, 0) + 1
+
+        # Prepare user_data with actual values
+        user_data_formatted = config['aws']['EC2']['user_data'].format(
+            NamePrefix=config['aws']['NamePrefix'] + f"VM{vm_count[region]}",
             panorama_api_key=config['palo_alto']['panorama']['api_key'],
             panorama_ip_address1=config['palo_alto']['panorama']['ip_address1'],
             panorama_ip_address2=config['palo_alto']['panorama']['ip_address2'],
             PanoramaTemplate=config['palo_alto']['panorama']['PanoramaTemplate'],
             PanoramaDeviceGroup=config['palo_alto']['panorama']['PanoramaDeviceGroup']
         )
-        user_data_encoded = base64.b64encode(user_data_raw.encode()).decode()
-        print(f'UserData: {user_data_encoded}')
+        # Convert user data to single line, semicolon-separated
+        user_data_semi_colon_separated = user_data_formatted.replace('\n', ';')
+        print(f'User-Data Formated: {user_data_formatted}')
+        print(f'User-Data Resolved: {user_data_semi_colon_separated}')
+
+        # Base64 encode the user data
+        user_data_encoded = base64.b64encode(user_data_semi_colon_separated.encode()).decode()
+
         # Create Security Groups
         sg_public_id, sg_private_id = create_security_groups(ec2_client, region_outputs['VpcId'], config['aws']['NamePrefix'])
 
-        # Create Network Interfaces
-        ni1_id = create_network_interface(ec2_client, region_outputs['Subnet1Id'], sg_public_id)
-        ni2_id = create_network_interface(ec2_client, region_outputs['Subnet2Id'], sg_private_id)
-        ni3_id = create_network_interface(ec2_client, region_outputs['Subnet2Id'], sg_private_id)
+        # Define network interfaces configuration
+        network_interfaces_config = [
+            {'SubnetId': region_outputs['Subnet1Id'], 'SecurityGroupId': sg_public_id},
+            {'SubnetId': region_outputs['Subnet2Id'], 'SecurityGroupId': sg_private_id},
+            {'SubnetId': region_outputs['Subnet2Id'], 'SecurityGroupId': sg_private_id}
+        ]
 
-        # Create Elastic IP and associate it with the first network interface
-        eip_alloc_id1 = create_elastic_ip(ec2_client)
-        eip_alloc_id2 = create_elastic_ip(ec2_client)
-        associate_elastic_ip(ec2_client, eip_alloc_id1, ni1_id)
-        associate_elastic_ip(ec2_client, eip_alloc_id1, ni2_id)
-
-        # Deploy EC2 instance
-        user_data_encoded = base64.b64encode(config['aws']['EC2']['user_data'].encode()).decode()
+        # EC2 Settings
         ami_id = region_config['ngfw_ami_id']
         instance_type = config['aws']['EC2']['instance_type']
         key_name = region_config['key_name']
+        
+        # Define VM name
+        vm_name = f"{config['aws']['NamePrefix']}VM{vm_count[region]}"
 
+        # Deploy EC2 instance
         instance_id = deploy_ec2_instance(
             ec2_client,
             ami_id,
             instance_type,
             key_name,
             user_data_encoded,
-            [{'NetworkInterfaceId': ni1_id, 'DeviceIndex': 0},
-             {'NetworkInterfaceId': ni2_id, 'DeviceIndex': 1},
-             {'NetworkInterfaceId': ni3_id, 'DeviceIndex': 2}]
-        )       
+            network_interfaces_config,
+            vm_name
+        )
+        # Wait for instance to be in running state (optional)
+        ec2_client.get_waiter('instance_running').wait(InstanceIds=[instance_id])
+
+        # Create and associate Elastic IPs to the first two network interfaces
+        instance_info = ec2_client.describe_instances(InstanceIds=[instance_id])
+        network_interface_ids = [
+            ni['NetworkInterfaceId'] for ni in instance_info['Reservations'][0]['Instances'][0]['NetworkInterfaces']
+        ]
+
+        eip_alloc_ids = []
+        for ni_id in network_interface_ids[:2]:  # Limit to the first two interfaces
+            eip_alloc_id = create_elastic_ip(ec2_client)
+            associate_elastic_ip(ec2_client, eip_alloc_id, ni_id)
+            eip_alloc_ids.append(eip_alloc_id)
+
         print(f"EC2 Instance created in {region}: {instance_id}")
+
         # Record the created resources in the state dictionary
         state[region] = {
             'SecurityGroups': {'Public': sg_public_id, 'Private': sg_private_id},
-            'NetworkInterfaces': [ni1_id, ni2_id, ni3_id],
-            'ElasticIPs': [eip_alloc_id1, eip_alloc_id2],
-            'EC2InstanceId': instance_id
+            'EC2InstanceId': instance_id,
+            'NetworkInterfaces': network_interface_ids,  # Add network interface IDs
+            'ElasticIPs': eip_alloc_ids
         }
 
     # Write the state information to a file
